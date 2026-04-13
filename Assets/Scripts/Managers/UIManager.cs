e4f53d49b71b699922f86a1b7e4d7a01b4f38751
Adam Tuck <adamtuck@gmail.com>
2026-04-13 14:25:32 -0700

    UI Clock - Clock overlayed and updates with in-game time & can be enabled/disabled

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DistantLands.Cozy;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private bool gamePaused;
+    [SerializeField] private bool clockVisible;
 
     [Header("UIMessages")]
     [SerializeField] private GameObject pauseScreenObj;
+
+    [Header("Object Refs")]
+    [SerializeField] private GameObject uiClockObject;
 
     // Singleton
     public static UIManager instance;
@@ -19,6 +26,59 @@ public class UIManager : MonoBehaviour
         }
 
         instance = this;
+    }
+
+    private void Update()
+    {
+        UpdateUIClock();
+    }
+
+    private void UpdateUIClock()
+    {
+        if (clockVisible)
+        {
+            uiClockObject.SetActive(true);
+
+            int hours, minutes;
+            string hoursText, minutesText, amPmText;
+
+            hours = CozyWeather.instance.timeModule.currentTime.hours;
+            minutes = CozyWeather.instance.timeModule.currentTime.minutes;
+
+            // Converts to 12hr time and switches AM/PM
+            if (hours > 12)
+            {
+                hours -= 12;
+                hoursText = hours.ToString();
+                amPmText = "PM";
+            }
+            else if (hours == 0)
+            {
+                hours = 12;
+                hoursText = hours.ToString();
+                amPmText = "AM";
+            }
+            else if (hours == 12)
+            {
+                hoursText = hours.ToString();
+                amPmText = "PM";
+            }
+            else
+            {
+                hoursText = hours.ToString();
+                amPmText = "AM";
+            }
+
+            // Adds leading minutes zero
+            if (minutes < 10)
+                minutesText = "0" + minutes.ToString();
+            else
+                minutesText = minutes.ToString();
+
+            uiClockObject.GetComponentInChildren<TextMeshProUGUI>().text = hoursText + ":" + minutesText + " " + amPmText;
+        }
+        else
+            uiClockObject.SetActive(false);
     }
 
     public void PauseGameToggle()
