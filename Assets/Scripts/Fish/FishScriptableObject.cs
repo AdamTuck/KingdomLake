750687f95d985b0aad788793e37f6bd49777e113
Adam Tuck <adamtuck@gmail.com>
2026-04-30 11:29:33 -0700

    Fish scriptable objects - Updated fish scriptable object to have any parameters at all - Updated low poly fish with more options - gave the character a fishing rod

--- Assets/Scripts/Fish/FishScriptableObject.cs
+++ Assets/Scripts/Fish/FishScriptableObject.cs
@@ -1,7 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "FishObject", menuName = "Scriptable Objects/FishObject")]
 public class FishScriptableObject : ScriptableObject
 {
-    public DialogueLine[] dialogueLines;
+    [Header("Fish Properties")]
+    public string fishName;
+    public GameObject fishPrefab;
+    public FishType fishType;
+
+    [Header("Location")]
+    public Lakes lakes;
+    public float minDepth;
+    public float maxDepth;
+
+    [Header("Capitalism")]
+    public float sellPriceLow;
+    public float sellPriceHigh;
+}
+
+[Flags]
+public enum Lakes
+{
+    Lake1,
+    Lake2,
+    Lake3,
+    Lake4,
+    Lake5
+}
+
+public enum FishType
+{
+    Common,
+    Special
 }
